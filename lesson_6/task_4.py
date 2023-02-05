e355eb924f821d80d302b4515f390f14f3f7a28d
Alekseeva332 <84273151+Alekseeva332@users.noreply.github.com>
2023-02-05 23:25:39 +0300

    функция для вывода текущего времени с задержкой в 1 секунду

--- lesson_6/task_4.py
+++ lesson_6/task_4.py
@@ -2,14 +2,28 @@ from datetime import datetime
 import time
 
 
-def func(n):
-    list1 = []
-    for i in range(n):
-        list1.append(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'))
-        time.sleep(1)
-    return list1
+def sleep_time() -> str:
+    time_now = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+    time.sleep(1)
+    return time_now
 
 
-result = func(int(input('Введите число: ')))
-print(result)
+def range_number(n: int) -> list:
+    number = [sleep_time() for _ in range(n)]
+    return number
 
+
+result = int(input('Введите число: '))
+print(range_number(result))
+
+
+# def func(n):
+#     list1 = []
+#     for i in range(n):
+#         list1.append(datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S'))
+#         time.sleep(1)
+#     return list1
+#
+#
+# result = func(int(input('Введите число: ')))
+# print(result)
